2c4c71f0ab66eafadb5f2f3deb9c1a4819538d26
antonio160698 <antonio.1606.98@gmail.com>
2020-05-28 02:08:22 -0500

    Se hicieron los links de los htmls y se hizo el diseño de la pagina principal

--- app.py
+++ app.py
@@ -4,8 +4,28 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     title = "Home"
-    lista = ["h", "hsugu", "nkhq"]
-    return render_template("index.html", title=title, lista = lista)
+    lista = [{"Nombre":"Antonio", "Placa":"8989898","Hora":"", "img":"https://i0.wp.com/grupometropoli.net/wp-content/uploads/2018/11/placas-de-nuevo-leon.jpg?fit=700%2C349&ssl=1"},
+    {"Nombre":"Jose", "Placa":"8358240","Hora":"", "img":"https://i0.wp.com/grupometropoli.net/wp-content/uploads/2018/11/placas-de-nuevo-leon.jpg?fit=700%2C349&ssl=1"},
+    {"Nombre":"Angel", "Placa":"3874983","Hora":"", "img":"https://i0.wp.com/grupometropoli.net/wp-content/uploads/2018/11/placas-de-nuevo-leon.jpg?fit=700%2C349&ssl=1"}]
+    return render_template("index.html", 
+    title=title, 
+    lista = lista,
+    listalen = len(lista))
+@app.route("/crear")
+def crear():
+    return render_template("crear.html")
+
+@app.route("/Graficas")
+def grafica():
+    return render_template("graficas.html")
+
+@app.route("/Ingresar")
+def ingresar():
+    return render_template("ingresar.html")
+
+@app.route("/Dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 from routes.google import *
 
