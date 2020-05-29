0d1d8893fb36cf68b9f780e2c34e794368b3140a
antonio160698 <antonio.1606.98@gmail.com>
2020-05-29 15:57:49 -0500

    Se hizo el diseño de las pantallas y se cambiaron cositas falta detalles

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     title = "Home"
-    lista = [{"Nombre":"Antonio", "Placa":"8989898","Hora":"", "img":"https://i0.wp.com/grupometropoli.net/wp-content/uploads/2018/11/placas-de-nuevo-leon.jpg?fit=700%2C349&ssl=1"},
-    {"Nombre":"Jose", "Placa":"8358240","Hora":"", "img":"https://i0.wp.com/grupometropoli.net/wp-content/uploads/2018/11/placas-de-nuevo-leon.jpg?fit=700%2C349&ssl=1"},
-    {"Nombre":"Angel", "Placa":"3874983","Hora":"", "img":"https://i0.wp.com/grupometropoli.net/wp-content/uploads/2018/11/placas-de-nuevo-leon.jpg?fit=700%2C349&ssl=1"}]
+    lista = [
+    {"Nombre":"Antonio", "Placa":"k56-afk","Hora":"", "img":"https://cdn-3.expansion.mx/dims4/default/bd2f85d/2147483647/strip/true/crop/1200x801+0+0/resize/800x534!/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Ff7%2F6a%2Fd2dc8c124361883a407bc7bc0e4d%2Fcambio-placas-1.jpg-1%20%281%29.jpg"},
+    {"Nombre":"Jose", "Placa":"gza-39-94","Hora":"", "img":"https://media.metrolatam.com/2019/06/03/placas-e15245428559abe9a67bb7a0d9b96204-1200x800.jpg"},
+    {"Nombre":"Angel", "Placa":"unk-028","Hora":"", "img":"https://live.staticflickr.com/8002/7446223590_a650a0d7e5_b.jpg"}]
     return render_template("index.html", 
     title=title, 
     lista = lista,
@@ -26,6 +27,20 @@ def ingresar():
 @app.route("/Dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
+@app.route("/iframe/Dashboard")
+def iframedash():
+    return render_template("frames/iframegraphs.html")
+
+@app.route("/iframe/list")
+def iframelist():
+    lista = [
+    {"Nombre":"Antonio", "Placa":"k56-afk","Hora":"", "img":"https://cdn-3.expansion.mx/dims4/default/bd2f85d/2147483647/strip/true/crop/1200x801+0+0/resize/800x534!/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Ff7%2F6a%2Fd2dc8c124361883a407bc7bc0e4d%2Fcambio-placas-1.jpg-1%20%281%29.jpg"},
+    {"Nombre":"Jose", "Placa":"gza-39-94","Hora":"", "img":"https://media.metrolatam.com/2019/06/03/placas-e15245428559abe9a67bb7a0d9b96204-1200x800.jpg"},
+    {"Nombre":"Angel", "Placa":"unk-028","Hora":"", "img":"https://live.staticflickr.com/8002/7446223590_a650a0d7e5_b.jpg"}]
+    return render_template("frames/iframelist.html", 
+    lista = lista,
+    listalen = len(lista))
 
 from routes.google import *
 
