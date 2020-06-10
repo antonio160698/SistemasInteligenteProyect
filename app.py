9807874dbc30ec6dd6ac09dedf8c136029deef11
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-06-10 01:55:20 -0500

    feat: Add show action to render show and edit the object

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ def index():
     title=title, 
     lista = lista,
     listalen = len(lista))
+
+@app.route("/reports/<id>")
+def show(id):
+    objects_list = [
+    {"Nombre":"Jose Antonio Torres Garibay", "Zona": "A6", "Placa":"K56-AFK", "Fecha":"10/06/2020", "img":"https://cdn-3.expansion.mx/dims4/default/bd2f85d/2147483647/strip/true/crop/1200x801+0+0/resize/800x534!/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Ff7%2F6a%2Fd2dc8c124361883a407bc7bc0e4d%2Fcambio-placas-1.jpg-1%20%281%29.jpg"},
+    {"Nombre":"Jose Miguel Salazar Ramirez", "Zona": "C22", "Placa":"GZA-39-94", "Fecha":"10/06/2020", "img":"https://media.metrolatam.com/2019/06/03/placas-e15245428559abe9a67bb7a0d9b96204-1200x800.jpg"},
+    {"Nombre":"Angel Abdiel Jimenez Garza", "Zona": "D3", "Placa":"UNK-028", "Fecha":"11/06/2020", "img":"https://live.staticflickr.com/8002/7446223590_a650a0d7e5_b.jpg"}]
+    return render_template("show.html", object=objects_list[int(id)])
+
 @app.route("/reports/new")
 def crear():
     return render_template("crear.html")
